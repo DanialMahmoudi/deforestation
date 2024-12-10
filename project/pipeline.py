0b7b00018d0db820ce9bed9ec8b8f4d12795ba32
Danial Mahmoudi <danialmahmoudi21@gmail.com>
2024-12-10 16:51:04 +0100

    Test the workflow

--- project/pipeline.py
+++ project/pipeline.py
@@ -13,7 +13,7 @@ import seaborn as sns
 
 
 # To see whether CI works or not
-
+#
 # Detect virtual environment path dynamically
 venv_path = os.path.join(sys.prefix, 'Scripts', 'kaggle.exe') if os.name == 'nt' else os.path.join(sys.prefix, 'bin', 'kaggle')
 
